9310141bcf1cd8200d8acfd130447a195bde18a9
Roy Zuo <me@roylez.info>
2025-05-14 15:25:45 +1000

    minor changes to <cr> mapping in normal mode

--- plugin/zwiki.lua
+++ plugin/zwiki.lua
@@ -37,11 +37,7 @@ vim.api.nvim_create_autocmd("BufEnter", {
     if autocmd_callback() then
       vim.keymap.set('i', '[[', "<esc>:lua require('zwiki').link()<CR>", {buffer=true})
       vim.keymap.set('v', 'z', 'y:ZwikiTab<CR>p', {buffer=true})
-      vim.keymap.set('n', '<CR>',
-        function()
-          require('zwiki').open_link()
-        end,
-        {noremap=true, buffer=true, silent=true})
+      vim.keymap.set('n', '<CR>', require('zwiki').open_link, {buffer=true, silent=true})
     end
   end
 })
